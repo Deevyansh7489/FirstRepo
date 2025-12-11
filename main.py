5fcc201ea796198cba25642f4a8b557b59c634b6
Deevyansh7489 <deevyansh.dewangan@wisseninfotech.com>
2025-12-11 18:47:36 +0530

    feat(): add hello from Deevyansh

--- main.py
+++ main.py
@@ -5,3 +5,7 @@ app = FastAPI()
 @app.get("/")
 def app_func():
     return {"message": "Hello, World!"}
+
+@app.get("/hello-deevyansh")
+async def hello_deevyansh():
+    return {"message": "Hello from Deevyansh!"}
